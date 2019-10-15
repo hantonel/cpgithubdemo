184516b584480f1177a3d82956dbf693533273a4
pssabato <52446944+pssabato@users.noreply.github.com>
2019-10-15 21:01:58 +0100

    Add

---
+++ set_gb_localisation_and_tz.ps1
@@ -0,0 +1,28 @@
+Expand-Archive -Path "C:\Users\Public\Downloads\language.zip" C:\Users\Public\Downloads\language
+
+$DefaultHKEY = "HKU\DEFAULT_USER"
+$DefaultRegPath = "C:\Users\Default\NTUSER.DAT"
+
+Set-TimeZone -Name "GMT Standard Time"
+Set-Culture en-GB
+Set-WinSystemLocale en-GB
+Set-WinHomeLocation -GeoId 242
+Set-WinUserLanguageList en-GB -Force
+Set-WinUILanguageOverride -Language en-GB
+
+reg load $DefaultHKEY $DefaultRegPath
+reg import "C:\Users\Public\Downloads\language\en-gb-default.reg"
+reg unload $DefaultHKEY
+reg import "C:\Users\Public\Downloads\language\en-gb-welcome.reg"
+
+reg load "HKU\TEMP" "C:\Users\Default\NTUSER.DAT"
+REG IMPORT "C:\Users\Public\Downloads\language\default.REG"
+REG UNLOAD "HKU\TEMP"
+
+REG IMPORT "C:\Users\Public\Downloads\language\s-1-5-19.REG"
+
+REG IMPORT "C:\Users\Public\Downloads\language\s-1-5-20.REG"
+
+REG IMPORT "C:\Users\Public\Downloads\language\.default.REG"
+
+Start-Sleep -Seconds 180 ; Restart-Computer -Force
